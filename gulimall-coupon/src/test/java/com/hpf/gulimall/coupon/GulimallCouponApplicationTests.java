4585893901027079f96e5cb025cc475505dd4e10
黄鹏飞 <641655770@qq.com>
2021-11-24 15:54:33 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/hpf/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpf.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-member/src/test/java/com/hpf/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpf.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
