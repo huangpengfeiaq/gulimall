1ee89614c745bcf3385449124f791d7c611748b9
huangpengfei <641655770@qq.com>
2022-03-04 22:29:43 +0800

    [update]

--- gulimall-product/src/main/java/com/hpf/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/hpf/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.hpf.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +32,37 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2.组装成父子的树形结构
+
+        //2.1).找到所有的一级分类
+        List<CategoryEntity> level1Menus = entities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .peek(menu -> menu.setChildren(getChildren(menu, entities)))
+//                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return level1Menus;
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     *
+     * @param root
+     * @param all
+     * @return
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream()
+                .filter(categoryEntity -> Objects.equals(categoryEntity.getParentCid(), root.getCatId()))
+                //1.找到子菜单
+                .peek(categoryEntity -> categoryEntity.setChildren(getChildren(categoryEntity, all)))
+                //2.菜单的排序
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return children;
+    }
 }
