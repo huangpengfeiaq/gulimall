362dfd76bfd72f3b5cec0f3a1c53a5ebf8d3a05e
huangpengfei <641655770@qq.com>
2022-03-25 22:34:17 +0800

    [update]

--- gulimall-product/src/main/java/com/hpf/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/hpf/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -11,6 +11,7 @@ import com.hpf.common.utils.Query;
 import com.hpf.gulimall.product.dao.SkuInfoDao;
 import com.hpf.gulimall.product.entity.SkuInfoEntity;
 import com.hpf.gulimall.product.service.SkuInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("skuInfoService")
@@ -29,6 +30,38 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCodition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.and(w -> w.eq("sku_id", key).or().like("sku_name", key));
+        }
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equals(brandId)) {
+            queryWrapper.eq("brand_id", brandId);
+        }
+        String catelogId = (String) params.get("catelogId");
+        if (!StringUtils.isEmpty(catelogId) && !"0".equals(catelogId)) {
+            queryWrapper.eq("catalog_id", catelogId);
+        }
+        String min = (String) params.get("min");
+        if (!StringUtils.isEmpty(min)) {
+            queryWrapper.ge("price", min);
+        }
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(max)) {
+            queryWrapper.le("price", max);
+        }
+
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),queryWrapper
+        );
+
+        return new PageUtils(page);
     }
 
 }
