e0ae724c5219caf6b9ebf6f54e9c5d1f4703b394
huangpengfei <641655770@qq.com>
2022-03-26 21:44:24 +0800

    [deepin]

--- gulimall-ware/src/main/java/com/hpf/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/hpf/gulimall/ware/GulimallWareApplication.java
@@ -1,8 +1,14 @@
 package com.hpf.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.hpf.gulimall.ware.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallWareApplication {
 
