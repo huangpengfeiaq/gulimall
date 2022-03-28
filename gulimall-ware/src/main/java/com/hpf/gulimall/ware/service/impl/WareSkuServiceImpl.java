80d32812feb3a91650a2542af54e84e8f4fa0e04
huangpengfei <641655770@qq.com>
2022-03-28 14:14:58 +0800

    [deepin]

--- gulimall-ware/src/main/java/com/hpf/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/hpf/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hpf.gulimall.ware.service.impl;
 
+import com.hpf.gulimall.ware.entity.WareInfoEntity;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,6 +12,7 @@ import com.hpf.common.utils.Query;
 import com.hpf.gulimall.ware.dao.WareSkuDao;
 import com.hpf.gulimall.ware.entity.WareSkuEntity;
 import com.hpf.gulimall.ware.service.WareSkuService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareSkuService")
@@ -18,9 +20,19 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if (!StringUtils.isEmpty(skuId)) {
+            queryWrapper.eq("sku_id", skuId);
+        }
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)) {
+            queryWrapper.eq("ware_id", wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
