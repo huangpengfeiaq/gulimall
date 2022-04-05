e52633c75350430f6fd4edd4a1502e008272c86b
huangpengfei <641655770@qq.com>
2022-04-05 11:21:47 +0800

    [deepin]新增elasticsearch模块

---
+++ gulimall-search/src/main/java/com/hpf/gulimall/search/config/ElasticSearchConfig.java
@@ -0,0 +1,17 @@
+package com.hpf.gulimall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.56.10", 9200, "http")));
+    }
+}
