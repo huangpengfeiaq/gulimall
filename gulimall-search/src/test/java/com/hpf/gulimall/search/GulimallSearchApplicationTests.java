348a3384b5eb7af4702fc057ef4e590ad567498c
huangpengfei <641655770@qq.com>
2022-04-05 17:32:06 +0800

    [deepin]新增elasticsearch模块

--- gulimall-search/src/test/java/com/hpf/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/hpf/gulimall/search/GulimallSearchApplicationTests.java
@@ -19,6 +19,21 @@ public class GulimallSearchApplicationTests {
     @Autowired
     private RestHighLevelClient client;
 
+    @Data
+    static class Account {
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+    }
+
     /**
      * 测试存储到es
      */
