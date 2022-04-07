a07924d66a373807f0afdb570828fee6b134cee3
huangpengfei <641655770@qq.com>
2022-04-07 14:26:06 +0800

    [deepin]商城业务-首页

--- gulimall-product/src/main/java/com/hpf/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/hpf/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.hpf.gulimall.product.service.impl;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.hpf.gulimall.product.dao.CategoryBrandRelationDao;
 import com.hpf.gulimall.product.entity.CategoryBrandRelationEntity;
+import com.hpf.gulimall.product.vo.Catelog2Vo;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -113,4 +114,53 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
                 .collect(Collectors.toList());
         return children;
     }
+
+    //    @Cacheable(value = {"category"}, key = "#root.method.name")//当前方法的结果需要缓存
+    @Override
+    public List<CategoryEntity> getLevel1() {
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", 0));
+        return categoryEntities;
+    }
+
+    //    @Cacheable(value = "category",key = "#root.methodName")
+    @Override
+    public Map<String, List<Catelog2Vo>> getCatelogJson() {
+        /*
+          优化:将数据库中的多次查询变为一次,存至缓存selectList,需要的数据从list取出,避免频繁的数据库交互
+         */
+        List<CategoryEntity> selectList = baseMapper.selectList(null);
+        //1.查出所有1级分类
+        List<CategoryEntity> level1 = getParent_cid(selectList, 0L);
+        //2.封装数据
+        Map<String, List<Catelog2Vo>> parent_cid = level1.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
+                    //1.查出1级分类中所有2级分类
+                    List<CategoryEntity> categoryEntities = getParent_cid(selectList, v.getCatId());
+                    //2.封装上面的结果
+                    List<Catelog2Vo> catelog2Vos = null;
+                    if (categoryEntities != null) {
+                        catelog2Vos = categoryEntities.stream().map(l2 -> {
+                            Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName());
+                            //查询当前2级分类的3级分类
+                            List<CategoryEntity> level3 = getParent_cid(selectList, l2.getCatId());
+                            if (level3 != null) {
+                                List<Catelog2Vo.Catelog3Vo> collect = level3.stream().map(l3 -> {
+                                    //封装指定格式
+                                    return new Catelog2Vo.Catelog3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
+                                }).collect(Collectors.toList());
+                                catelog2Vo.setCatalog3List(collect);
+                            }
+                            return catelog2Vo;
+                        }).collect(Collectors.toList());
+                    }
+                    return catelog2Vos;
+                }
+        ));
+        return parent_cid;
+    }
+
+    private List<CategoryEntity> getParent_cid(List<CategoryEntity> selectList, Long parent_cid) {
+        List<CategoryEntity> collect = selectList.stream().filter(item -> Objects.equals(item.getParentCid(), parent_cid)).collect(Collectors.toList());
+        return collect;
+    }
+
 }
