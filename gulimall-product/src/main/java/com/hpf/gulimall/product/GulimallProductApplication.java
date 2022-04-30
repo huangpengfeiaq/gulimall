64d4d937dd376e26c09786351fb14c25b8616318
huangpengfei <641655770@qq.com>
2022-04-30 20:00:02 +0800

    [win11]商城业务-认证服务

--- gulimall-product/src/main/java/com/hpf/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/hpf/gulimall/product/GulimallProductApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 1、整合MyBatis-Plus
@@ -57,7 +58,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *  1）、编写异常处理类，使用@ControllerAdvice。
  *  2）、使用@ExceptionHandler标注方法可以处理的异常。
  */
-//@EnableRedisHttpSession     //开启springsession
+@EnableRedisHttpSession     //开启springSession
 @EnableCaching      //开启缓存功能
 @EnableFeignClients(clients = {CouponFeignClient.class, WareFeignClient.class, SearchFeignClient.class})
 @EnableDiscoveryClient
