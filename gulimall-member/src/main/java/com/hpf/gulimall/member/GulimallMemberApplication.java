e6132ed8532247a1b6888fc99774ed776d085a32
huangpengfei <641655770@qq.com>
2022-05-22 12:38:40 +0800

    [win11]商城业务-订单服务

--- gulimall-member/src/main/java/com/hpf/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/hpf/gulimall/member/GulimallMemberApplication.java
@@ -6,7 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+/**
+ * 1、spring-session依赖
+ * 2、spring-session配置
+ * 3、LoginInterceptor拦截器
+ */
+@EnableRedisHttpSession
 @EnableFeignClients(clients = {CouponFeignClient.class, OrderFeignClient.class})
 @EnableDiscoveryClient
 @SpringBootApplication
